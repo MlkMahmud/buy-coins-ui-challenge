2b7957476e4e46b195cf224a3535a2702cd79ad5
Malik Mahmud <almalikmahmud@gmail.com>
2019-12-08 21:40:50 +0100

    Integrate API

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-/*
- * ✅ Use the Coinlore API (Coins)
- *    https://www.coinlore.com/cryptocurrency-data-api
- *
- *    Get 10 coins per "page"
- */
+const url = 'https://api.coinlore.com/api/tickers/?start=1&limit=10';
+const tBody = document.querySelector('tbody');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+let page = 0;
+
+const generateRow = ({symbol, name, price_usd, tsupply}) => {
+  const row = document.createElement('tr');
+  row.innerHTML = `<td>${name}</td>
+  <td>${symbol}</td>
+  <td>$ ${price_usd}</td>
+  <td>${tsupply} ${symbol}</td>`;
+  return row;
+};
+
+const appendRow = (element, row) => (element.appendChild(row)); 
+
+window.addEventListener('load', async () => {
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+  const { data } = json;
+  data.forEach(item => {
+    appendRow(tBody, generateRow(item));
+  });
+});
